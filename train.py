fb26d354eec7561ca2398072ad4ac20ffcfb4f25
Sakshi Shah <sakshishahjs12@gmail.com>
2026-07-23 13:19:37 -0500

    changes in dataloader

--- train.py
+++ train.py
@@ -156,22 +156,17 @@ def get_ds(config):
     print(f"Max length of target sentence: {max_len_tgt}")
 
     train_dataloader = DataLoader(
-    train_ds,
-    batch_size=config["batch_size"],
-    shuffle=True,
-    num_workers=2,
-    pin_memory=True,
-    persistent_workers=True
-)
+        train_ds,
+        batch_size=config["batch_size"],
+        shuffle=True,
+    )
 
     val_dataloader = DataLoader(
-    val_ds,
-    batch_size=1,
-    shuffle=False,
-    num_workers=2,
-    pin_memory=True,
-    persistent_workers=True
-)
+        val_ds,
+        batch_size=1,
+        shuffle=True,
+    )
+
     return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
 
 def get_model(config,vocab_src_len,vocab_tgt_len):
@@ -179,132 +174,70 @@ def get_model(config,vocab_src_len,vocab_tgt_len):
     return model
 
 def train_model(config):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"Using device {device}")
+    device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'Using device {device}')
 
-    Path(config["model_folder"]).mkdir(parents=True, exist_ok=True)
+    Path(config['model_folder']).mkdir(parents=True,exist_ok=True)
 
-    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
+    train_dataloader,val_dataloader,tokenizer_src,tokenizer_tgt=get_ds(config)
+    model=get_model(config,tokenizer_src.get_vocab_size(),tokenizer_tgt.get_vocab_size()).to(device)
 
-    model = get_model(
-        config,
-        tokenizer_src.get_vocab_size(),
-        tokenizer_tgt.get_vocab_size()
-    ).to(device)
+    writer = SummaryWriter(config['experiment_name'])
 
-    writer = SummaryWriter(config["experiment_name"])
+    optimizer=torch.optim.Adam(model.parameters(),lr=config['lr'],eps=1e-9)
 
-    optimizer = torch.optim.Adam(
-        model.parameters(),
-        lr=config["lr"],
-        eps=1e-9
-    )
+    initial_epoch=0
+    global_step=0
+    if config['preload']:
+        model_filename=get_weights_file_path(config,config['preload'])
+        print(f'Preloading model {model_filename}')
+        state=torch.load(model_filename)
+        initial_epoch=state['epoch']+1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step=state['global_step']
+    
+    loss_fn=nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'),label_smoothing=0.1).to(device)
 
-    # Mixed Precision
-    scaler = torch.amp.GradScaler("cuda")
-
-    initial_epoch = 0
-    global_step = 0
-
-    if config["preload"]:
-        model_filename = get_weights_file_path(config, config["preload"])
-        print(f"Preloading model {model_filename}")
-
-        state = torch.load(model_filename)
-
-        model.load_state_dict(state["model_state_dict"])
-        optimizer.load_state_dict(state["optimize_state_dict"])
-
-        initial_epoch = state["epoch"] + 1
-        global_step = state["global_step"]
-
-    loss_fn = nn.CrossEntropyLoss(
-        ignore_index=tokenizer_src.token_to_id("[PAD]"),
-        label_smoothing=0.1
-    ).to(device)
-
-    for epoch in range(initial_epoch, config["num_epochs"]):
-
-        model.train()
-
-        batch_iterator = tqdm(
-            train_dataloader,
-            desc=f"Processing epoch {epoch:02d}"
-        )
-
+    for epoch in range(initial_epoch,config['num_epochs']):
+        
+        batch_iterator = tqdm(train_dataloader,desc=f'Processign epoch {epoch:02d}')
         for batch in batch_iterator:
+            model.train()
+            encoder_input=batch['encoder_input'].to(device)
+            decoder_input=batch['decoder_input'].to(device)
+            encoder_mask=batch['encoder_mask'].to(device)
+            decoder_mask=batch['decoder_mask'].to(device)
 
-            encoder_input = batch["encoder_input"].to(device, non_blocking=True)
-            decoder_input = batch["decoder_input"].to(device, non_blocking=True)
-            encoder_mask = batch["encoder_mask"].to(device, non_blocking=True)
-            decoder_mask = batch["decoder_mask"].to(device, non_blocking=True)
-            label = batch["label"].to(device, non_blocking=True)
+            encoder_output=model.encode(encoder_input,encoder_mask)
+            decoder_output=model.decode(encoder_output,encoder_mask,decoder_input,decoder_mask)
+            proj_output=model.project(decoder_output)
 
-            optimizer.zero_grad(set_to_none=True)
+            label=batch['label'].to(device)
 
-            # Automatic Mixed Precision
-            with torch.amp.autocast(device_type="cuda"):
+            loss=loss_fn(proj_output.view(-1,tokenizer_tgt.get_vocab_size()),label.view(-1))
+            batch_iterator.set_postfix({f"loss":f"{loss.item():6.3f}"})
 
-                encoder_output = model.encode(
-                    encoder_input,
-                    encoder_mask
-                )
+            writer.add_scalar('train loss', loss.item(),global_step)
+            writer.flush()
 
-                decoder_output = model.decode(
-                    encoder_output,
-                    encoder_mask,
-                    decoder_input,
-                    decoder_mask
-                )
+            loss.backward()
 
-                proj_output = model.project(decoder_output)
+            optimizer.step()
+            optimizer.zero_grad()
 
-                loss = loss_fn(
-                    proj_output.view(-1, tokenizer_tgt.get_vocab_size()),
-                    label.view(-1)
-                )
+            global_step+=1
 
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
+        run_validation(model,val_dataloader,tokenizer_src,tokenizer_tgt,config['seq_len'],device,lambda msg:batch_iterator.write(msg),global_step,writer)
 
-            batch_iterator.set_postfix(
-                {"loss": f"{loss.item():6.3f}"}
-            )
+        model_filename=get_weights_file_path(config,f'{epoch:02d}')
+        torch.save({
+            'epoch':epoch,
+            'model_state_dict':model.state_dict(),
+            'optimize_state_dict':optimizer.state_dict(),
+            'global_step':global_step
+        },model_filename)
 
-            writer.add_scalar(
-                "train loss",
-                loss.item(),
-                global_step
-            )
-
-            global_step += 1
-
-        writer.flush()
-
-        run_validation(
-            model,
-            val_dataloader,
-            tokenizer_src,
-            tokenizer_tgt,
-            config["seq_len"],
-            device,
-            lambda msg: batch_iterator.write(msg),
-            global_step,
-            writer,
-        )
-
-        model_filename = get_weights_file_path(
-            config,
-            f"{epoch:02d}"
-        )
-
-        torch.save(
-            {
-                "epoch": epoch,
-                "model_state_dict": model.state_dict(),
-                "optimize_state_dict": optimizer.state_dict(),
-                "global_step": global_step,
-            },
-            model_filename,
-        )
+if __name__=='__main__':
+    warnings.filterwarnings('ignore')
+    config = get_config()
+    train_model(config)
